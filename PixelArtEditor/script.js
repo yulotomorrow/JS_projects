56648a30025b41e570e798c9e6e7e879eee81dfd
yulotomorrow <78768193+yulotomorrow@users.noreply.github.com>
2022-01-08 08:34:33 -0500

    Version 1 with basic functionality

--- PixelArtEditor/script.js
+++ PixelArtEditor/script.js
@@ -1,4 +1,4 @@
-document.body.onload = addElement;
+document.body.onload = addElement();
 
 function addElement(){
 
@@ -7,15 +7,21 @@ function addElement(){
 	canvasDiv.appendChild(pixelDiv);
 
 	for(let i = 1; i <= (16*16); ++i){
-		let pixelSingle = document.createElement("div"); 
+		const pixelSingle = document.createElement("div"); 
 		pixelDiv.appendChild(pixelSingle);
-		pixelSingle.className = "pixel";		
+		pixelSingle.className = "pixel";
+		pixelSingle.addEventListener("click", (e) => e.target.style.background = "rgb(214, 175, 0)" );		
 	};	
-	
+
     pixelDiv.style.display = "grid";
 	pixelDiv.style.gridTemplateColumns = "repeat(16, 1fr)"
 	pixelDiv.style.height = "500px";
 	pixelDiv.style.width = "500px";   
-    pixelDiv.style.background = "white";
-	
+    pixelDiv.style.background = "rgb(25, 27, 31)";	
+	pixelDiv.style.border = "5px solid rgb(25, 27, 31)";
+
+	const resetButton = document.querySelector("button");
+	const pixel = document.querySelectorAll("div.pixel");
+	resetButton.addEventListener("click",function () {pixel.forEach((p) => {p.style.background = "white"})});
 };
+
